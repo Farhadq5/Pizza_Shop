721f0b76ca72538446a4826ab8b50b7126a270c6
Farhadq5 <Farhad.qurishy5@gmail.com>
2024-11-21 13:10:47 +0300

    i have removed the connection string

--- DataAccess/ConnectionToSql.cs
+++ DataAccess/ConnectionToSql.cs
@@ -12,7 +12,9 @@ namespace DataAccess
        private readonly string ConnectionString;
         public ConnectionToSql()
         {
-            ConnectionString = "Server=88.243.252.9,49170;Database=pizzashopDb;User Id=farhad;Password=ff;ConnectRetryCount=10;";
+
+            ConnectionString = "Here your connection string";
+
 
         }
 
