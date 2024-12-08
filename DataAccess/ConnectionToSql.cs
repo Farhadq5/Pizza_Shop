0429a992774e15f690b84f7cb8f8c30d7828d81d
farhad qurishy <farhad.qurishy5@gmail.com>
2024-12-08 14:17:12 +0300

    i have added the conection to sql class and report class

---
+++ DataAccess/ConnectionToSql.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess
+{
+    public abstract class ConnectionToSql
+    {
+       private readonly string ConnectionString;
+        public ConnectionToSql()
+        {
+            ConnectionString = "Server=95.10.198.33,49170;Database=pizzashopDb;User Id=farhad;Password=ff;TrustServerCertificate=True;Connect Timeout=30;Encrypt=False;Max Pool Size=100;";
+
+        }
+
+        protected SqlConnection GetConnection()
+        {
+            return new SqlConnection(ConnectionString);
+        }
+    }
+}
